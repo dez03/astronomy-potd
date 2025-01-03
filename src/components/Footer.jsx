c98ae2e77d201e6a1d4587d1586ab9a24dc94cb8
dez03 <hernandezaviel@gmail.com>
2025-01-02 22:15:27 -0500

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="bgGradient"></div>
+      <div>
+          <h1>APOD PROJECT</h1>
+          <h2>Mars Brutal Landscape</h2>
+      </div>
+      <button >
+          <i className="fa-solid fa-circle-info"></i>
+      </button>
+    </footer>
+  )
+}
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,21 @@
+export default function SideBar() {
+  return (
+    <div className="sidebar">
+            <div className="bgOverlay"></div>
+            <div className="sidebarContents">
+                <h2>Mars Brutal Landscape</h2>
+                <div className="descriptionContainer">
+                    <p className="descriptionTitle">Description</p>
+                    <p>Lorem ipsum dolor sit amet consectetur 
+                      adipisicing elit. Illo quidem vitae minus, similique 
+                      aperiam doloribus enim ad officiis fugit amet quibusdam, 
+                      delectus iure maxime possimus tempora sint consectetur 
+                      exercitationem suscipit.</p>
+                </div>
+                <button>
+                    <i className="fa-solid fa-arrow-right"></i>
+                </button>
+            </div>
+        </div>
+  )
+}
